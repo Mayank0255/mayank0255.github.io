f6eeda70a8bac0607c1384bfd3a606c324b86a32
Mayank Aggarwal <mayank.aggarwal@zomato.com>
2024-06-02 01:17:52 +0530

    fix

--- tools/init.sh
+++ tools/init.sh
@@ -20,7 +20,7 @@ help() {
 
 check_status() {
   if [[ -n $(git status . -s) ]]; then
-    echo "Error: Commit unstaged files first, and then run this tool againt."
+    echo "Error: Commit unstaged files first, and then run this tool again."
     exit -1
   fi
 }
@@ -28,16 +28,14 @@ check_status() {
 check_init() {
   local _has_inited=false
 
-  if [[ ! -d docs ]]; then
-    if [[ ! -d .github ]]; then
-      _has_inited=true # --no-gh
-    else
-      if [[ -f .github/workflows/$ACTIONS_WORKFLOW ]]; then
-        # on BSD, the `wc` could contains blank
-        local _count="$(find .github/workflows/ -type f -name "*.yml" | wc -l)"
-        if [[ ${_count//[[:blank:]]/} == 1 ]]; then
-          _has_inited=true
-        fi
+  if [[ ! -d .github ]]; then # using option `--no-gh`
+    _has_inited=true
+  else
+    if [[ -f .github/workflows/$ACTIONS_WORKFLOW ]]; then
+      # on BSD, the `wc` could contains blank
+      local _count="$(find .github/workflows/ -type f -name "*.yml" | wc -l)"
+      if [[ ${_count//[[:blank:]]/} == 1 ]]; then
+        _has_inited=true
       fi
     fi
   fi
@@ -53,25 +51,14 @@ init_files() {
     rm -rf .github
   else
     ## Change the files of `.github`
-
     mv .github/workflows/$ACTIONS_WORKFLOW.hook .
     rm -rf .github
     mkdir -p .github/workflows
     mv ./${ACTIONS_WORKFLOW}.hook .github/workflows/${ACTIONS_WORKFLOW}
 
-    ## Ensure the gh-actions trigger branch
-
-    _workflow=".github/workflows/${ACTIONS_WORKFLOW}"
-    _default_branch="$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')"
-    _lineno="$(sed -n "/branches:/=" "$_workflow")"
-
-    sed -i.$TEMP_SUFFIX "$((_lineno + 1))s/- .*/- ${_default_branch}/" "$_workflow"
-    rm -f "$_workflow.$TEMP_SUFFIX"
-
     ## Cleanup image settings in site config
     sed -i.$TEMP_SUFFIX "s/^img_cdn:.*/img_cdn:/;s/^avatar:.*/avatar:/" _config.yml
     rm -f _config.yml.$TEMP_SUFFIX
-
   fi
 
   # trace the gem lockfile on user-end
@@ -79,12 +66,11 @@ init_files() {
   rm -f ".gitignore.$TEMP_SUFFIX"
 
   # remove the other fies
-  rm -f .travis.yml
-  rm -rf _posts/* docs
+  rm -rf _posts/*
 
   # save changes
   git add -A
-  git commit -m "[Automation] Initialize the environment." -q
+  git commit -m "chore: initialize the environment" -q
 
   echo "[INFO] Initialization successful!"
 }
@@ -98,19 +84,19 @@ _no_gh=false
 while (($#)); do
   opt="$1"
   case $opt in
-    --no-gh)
-      _no_gh=true
-      shift
-      ;;
-    -h | --help)
-      help
-      exit 0
-      ;;
-    *)
-      # unknown option
-      help
-      exit 1
-      ;;
+  --no-gh)
+    _no_gh=true
+    shift
+    ;;
+  -h | --help)
+    help
+    exit 0
+    ;;
+  *)
+    # unknown option
+    help
+    exit 1
+    ;;
   esac
 done
 
